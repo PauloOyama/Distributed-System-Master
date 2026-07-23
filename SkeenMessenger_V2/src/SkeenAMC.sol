9101cbaf7b1250cb7c0d1726f27c3628faf2674c
Paulo Oyama <papaloyama@gmail.com>
2026-07-23 16:18:56 -0300

    Start implementings

---
+++ SkeenMessenger_V2/src/SkeenAMC.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IMessageRecipient} from "@hyperlane-xyz/core/interfaces/IMessageRecipient.sol";
+import {SkeenMessenger} from "./SkeenMessenger.sol";
+
+contract SkeenAMC is IMessageRecipient {
+
+}
